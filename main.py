7239f8c9d9a595b8bdb19de1b1e7b5608802439d
ths1055 <fsr2103@kakao.com>
2023-02-25 22:07:29 +0900

    version 2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 '''
 ***Version 2***
 Project : ICE_Updater
-Developing Period : Feb. 06, 2023 ~ 
+Developing Period : Feb. 06, 2023 ~ Feb. 25, 2023
 Author : Ji-Hun Noh
-Date of last update: Jan. 30, 2023
+Date of last update: Feb. 25, 2023
 Update List :
     - v1.0 : Jan. 30, 2023
         -- Crawl announcement & article in college page, send that information to telegram
     - v2.0 : 
-        -- Filtering mechanism change
+        -- Data save and processing mechanism all rebuilt
 '''
 import http.client
 import json
@@ -78,7 +78,7 @@ class dataProcess(sendMSG):
         self.sheet=sheet
 
     def parser(self) -> None:
-        url='https://ice.yu.ac.kr/ice/board/notice.do'#환경변수
+        url=os.environ['yu_url']
         data=requests.get(url)
         bs=BeautifulSoup(data.text,'html.parser')
 
@@ -97,152 +97,180 @@ class dataProcess(sendMSG):
         
         for i, data in enumerate(data_article,len(self.parsing_list_announcement)+1):
             temp_list=[]
-            print(i)
             temp_list.append(data.select_one(f'#cms-content > div > div > div > div.bn-list-common01.type01.bn-common > table > tbody > tr:nth-child({i}) > td:nth-child(4)').text)
             temp_list.append(data.find('a').text.replace('\n',''))
             temp_list.append(url+data.find('a')['href'])
             self.parsing_list_article.append(temp_list)
 
-    def data_filtering(self) -> int:
+    def sheet_data_parsing(self) -> list:
         '''
-        case1: same data with last save data
-        case2: diffrent data with last save data
-        
-        renew_data_count case
-        0: announcement, article are None
-        1: article new data founded
-        10: announcemnet new data founded
-        11: both new data founded
+        지정된 스프레드 시트의 모든 데이터를 불러옵니다
         '''
-        renew_data_count=0
-        announcement_data_pos=0
-        article_data_pos=0
-
-        announcement_last_data=self.sheet.row_values(1)#announcement
-        if announcement_last_data[0] == self.parsing_list_announcement[0][0] and announcement_last_data[1] == self.parsing_list_announcement[0][1]:
-            print('Crawl data is same to Saved data : announcement')
-            renew_data_count+=0
-
-        else:
-            print('New data founded : announcement')
-            renew_data_count+=10
-            announcement_data_pos=self.data_pos_check(data_list=self.parsing_list_announcement, last_data=announcement_last_data)
+        announcement_data=self.sheet.get_all_values()
 
         self.set_sheet(self.article_sheet_url)
-        article_last_data=self.sheet.row_values(1)#article
-        if article_last_data[0] == self.parsing_list_article[0][0] and article_last_data[1] == self.parsing_list_article[0][1]:
-            print('Crawl data is same to Saved data : article')
-            renew_data_count+=0
+        artilce_data=self.sheet.get_all_values()
+
+        self.set_sheet(os.environ['announcement_today_url'])
+        anno_today_sheet_data=self.sheet.get_all_values()
+
+        self.set_sheet(os.environ['type_trans_sheet_url'])
+        type_trans_sheet_data = self.sheet.get_all_values()
+
+        return announcement_data, artilce_data, anno_today_sheet_data, type_trans_sheet_data
+
+    def data_pos_check(self, announcement_sheet_data, article_sheet_data) -> int:
+        '''
+        마지막으로 기록된 데이터의 위치를 확인하여
+        새로 불러온 데이터의 위치를 리턴합니다
+        ***2023.02.19 Error : announcement 데이터의 type이 차후에 trans됨에 따라 마지막 데이터의 날짜를 기준으로 데이터 slice 필요함***
+        -> clear
+        '''
+        primary_data_anno=announcement_sheet_data[0]
+        primary_data_article=article_sheet_data[0]
+        try:
+            primary_anno_data_pos=self.parsing_list_announcement.index(primary_data_anno)#갱신된 데이터(anno) 시작위치
+
+        except ValueError:
+            primary_anno_data_pos=None
+        primary_article_data_pos=self.parsing_list_article.index(primary_data_article)#갱신된 데이터(article) 시작위치
+
+        return primary_anno_data_pos, primary_article_data_pos
+
+    def data_type_trans_check(self, announcement_today_sheet_data:list) -> list:
+        '''
+        Announcement에서 내려간 article을 판별하여
+        list형태로 리턴합니다
+
+        data_pos_check보다 선행되어야 하는 작업입니다
+        '''
+        type_trans_list=[]
+        if self.parsing_list_announcement == announcement_today_sheet_data:
+            return None
 
         else:
-            print('New data founded : article')
-            renew_data_count+=1
-            article_data_pos = self.data_pos_check(data_list=self.parsing_list_article, last_data=article_last_data)
-        
-        self.set_sheet(self.announcement_sheet_url)
-
-        return renew_data_count, announcement_data_pos, article_data_pos, announcement_last_data, article_last_data
-
-    def data_verification(self, renew_data_count:int, announcement_pos:int, article_pos:int, announcement_last_data:list, article_last_data:list) -> str:
-        if renew_data_count == 10:
-            if self.data_check_sum(self.parsing_list_announcement, announcement_last_data, announcement_pos):
-                text_announcement=[]
-                for i in range(announcement_pos, -1, -1):
-                    self.data_saving(self.parsing_list_announcement[i])
-                    text_announcement_temp=self.set_send_text(type='Announcement', data_list=self.parsing_list_announcement[i])
-                    text_announcement.append(text_announcement_temp)
-                text_article=None
-            else:
-                print('Data checksum failed')
-
-            return text_announcement, text_article
-        
-        elif renew_data_count == 1:
-            if self.data_check_sum(self.parsing_list_announcement, announcement_last_data, announcement_pos):
-                text_article=[]
-                self.set_sheet(self.article_sheet_url)
-                for i in range(article_pos, -1, -1):
-                    self.data_saving(self.parsing_list_article[i])
-                    text_article_temp=self.set_send_text(type='Article', data_list=self.parsing_list_article[i])
-                    text_article.append(text_article_temp)
-                text_announcement=None
-                self.set_sheet(self.announcement_sheet_url)
-            else:
-                print('Data checksum failed')
-
-            return text_announcement, text_article
-        
-        elif renew_data_count == 11:
-            if self.data_check_sum(self.parsing_list_announcement, announcement_last_data, announcement_pos):
-                text_announcement=[]; text_article=[]
-                for i in range(announcement_pos, -1, -1):
-                    self.data_saving(self.parsing_list_announcement[i])
-                    text_announcement_temp=self.set_send_text(type='Announcement', data_list=self.parsing_list_announcement[i])
-                    text_announcement.append(text_announcement_temp)
-                self.set_sheet(self.article_sheet_url)
-                for i in range(article_pos, -1, -1):
-                    self.data_saving(self.parsing_list_article[i])
-                    text_article_temp=self.set_send_text(type='Article', data_list=self.parsing_list_article[i])
-                    text_article.append(text_article_temp)
-                self.set_sheet(self.announcement_sheet_url)
+            for i, data in enumerate(announcement_today_sheet_data):#data in announcement_today_sheet_data
+                if data not in self.parsing_list_announcement:
+                    type_trans_list.append(data)
             
-            else:
-                print('Data checksum failed')
+            return type_trans_list
 
-            return text_announcement, text_article
-
+    def date_calculation(self, primary_data_date:str) -> list:
         '''
-        if renew_data_count == 10:
-            self.data_saving(self.parsing_list_announcement[0])
-            text_announcement=self.set_send_text(type='Announcement', data_list=self.parsing_list_announcement[0])
-            text_article=None
-            return text_announcement, text_article
-
-        elif renew_data_count == 1:
-            self.set_sheet(self.article_sheet_url)
-            self.data_saving(self.parsing_list_article[0])
-            self.set_sheet(self.announcement_sheet_url)
-            text_article=self.set_send_text(type='Article', data_list=self.parsing_list_article[0])
-            text_announcement=None
-            return text_announcement, text_article
-
-        elif renew_data_count == 11:
-            self.data_saving(self.parsing_list_announcement[0])
-            text_announcement=self.set_send_text(type='Announcement', data_list=self.parsing_list_announcement[0])
-            self.set_sheet(self.article_sheet_url)
-            self.data_saving(self.parsing_list_article[0])
-            text_article=self.set_send_text(type='Article', data_list=self.parsing_list_article[0])
-            self.set_sheet(self.announcement_sheet_url)
-            return text_announcement, text_article
+        announcement의 글이 article로 type이 trans 된 경우 announcement에서는 식별이 되지 않기 때문에
+        날짜를 기준으로 판별해 그 위치를 리턴합니다
         '''
+        offset=[10000,100,1]
+        primary_map_data=map(int,primary_data_date.split(sep='.'))
+        primary_calc_data=0
+        for p, primary_data in enumerate(primary_map_data):
+            primary_calc_data+=primary_data*offset[p]
+
+        for i, data in enumerate(self.parsing_list_announcement):
+            map_data=map(int, data[0].split(sep='.'))
+            for j, data in enumerate(map_data):
+                #i값이 결국 해당 값 보다 더 많아지는 시점이니까 리턴해주면 됨
+                if primary_calc_data>=data*offset[j]:
+                    return i-1
+
+        raise ValueError
+                
+    def data_filtering(self, primary_anno_data_pos:int, primary_article_data_pos:int, primary_anno_data_date:str) -> list:
+        '''
+        매개변수로 주어진 마지막으로 기록된 데이터의 위치로
+        새로 추가된 데이터를 선별하여 list로 리턴합니다
+        '''
+        try:
+            filtering_anno_data=self.parsing_list_announcement[:primary_anno_data_pos]
+
+        except ValueError:
+            primary_calc_anno_data_pos=self.date_calculation(primary_anno_data_date)
+            filtering_anno_data=self.parsing_list_announcement[:primary_calc_anno_data_pos]
+            
+        filtering_article_data=self.parsing_list_article[:primary_article_data_pos]
+
+        return filtering_anno_data, filtering_article_data
+
+    def set_text_process(self, type_trans_list:list, filtering_anno_list:list, filtering_article_list:list) -> list:
+        '''
+        전송될 텍스트 데이터를 구성하고 리스트로 반환합니다.
+        '''
+        text_list=[]
+        try:
+            for trans_type in type_trans_list:
+                text=self.set_send_text('Type trans', 'Announcement -> Article', trans_type)
+                text_list.append(text)
+
+        except TypeError:
+            pass
+
+        try:
+            for anno_data in filtering_anno_list:
+                text=self.set_send_text('Normal', 'Announcement', anno_data)
+                text_list.append(text)
+
+        except TypeError:
+            pass
+
+        try:
+            for article_data in reversed(filtering_article_list):
+                text=self.set_send_text('Normal', 'Article', article_data)
+                text_list.append(text)
+
+        except TypeError:
+            pass
         
-    def data_pos_check(self, data_list:list, last_data:list):
-        return data_list.index(last_data)
+        return text_list
 
-    def data_check_sum(self, data_list:list, last_data:list, data_pos:int):
-        print()
-        #return 0 or 1
+    def msg_send_process(self, text_list) -> None:
+        for text in text_list:
+            param=self.set_param(text=text)
+            self.send_msg(param)
 
-    def msg_send_process(self, text_announcement:list, text_article:list):#리스트 형식 읽어서 msg sending 할 수 있게 modify해야함
-        if text_announcement != None and text_article != None:
-            param_anno=self.set_param(text=text_announcement)
-            param_arti=self.set_param(text=text_article)
-            self.send_msg(param_anno)
-            self.send_msg(param_arti)
+    def data_saving(self, anno_data_list:list, article_data_list:list, type_trans_list:list) -> None:
+        try:
+            self.set_sheet(self.announcement_sheet_url)
+            self.sheet.insert_rows(anno_data_list)
 
-        elif text_announcement != None and text_article == None:
-            param_anno=self.set_param(text=text_announcement)
-            self.send_msg(param_anno)
+        except TypeError:
+            print('New announcement data is not founded')
+            pass
 
-        elif text_announcement == None and text_article != None:
-            param_arti=self.set_param(text=text_article)
-            self.send_msg(param_arti)
+        try:
+            self.set_sheet(self.article_sheet_url)
+            self.sheet.insert_rows(article_data_list)
 
-    def data_saving(self, data:list) -> None:
-        self.sheet.insert_row(data)
+        except TypeError:
+            print('New Article data is not founded')
+            pass
+        
+        #anno_today -> not occur exception
+        self.set_sheet('https://docs.google.com/spreadsheets/d/16zlzptzXkF19d6w4mZVdYYNsfXnvPSuZhiKXTISIJUI')
+        self.sheet.clear()
+        self.sheet.insert_rows(self.parsing_list_announcement)
 
-    def set_send_text(self, type:str, data_list:list) -> str:
-        text=f'''Type : {type}
+        try:
+            self.set_sheet('https://docs.google.com/spreadsheets/d/1qsIrrD2Uu-hQRt33at9Drzm47Mv1i3D71XWlx_6c82M')
+            self.sheet.insert_rows(type_trans_list)
+
+        except TypeError:
+            print('New type trans data is not founded')
+            pass
+
+    def set_send_text(self, data_status:str , type:str, data_list:list) -> str:
+        if data_status == 'Type trans':
+            text=f'''Data status : {data_status}
+Type : {type}
+
+Date : {data_list[0]}
+
+Title : {data_list[1]}
+
+Url : {data_list[2]}
+            '''
+        else:   
+            text=f'''Data status : {data_status}
+Type : {type}
 
 Date : {data_list[0]}
 
@@ -254,14 +282,20 @@ Url : {data_list[2]}
 
     def routine(self):
         self.parser()
-        count, announcement_pos, article_pos, announcement_last_data, article_last_data=self.data_filtering()
-        if count != 0:
-            text_announcement, text_article=self.data_verification(count, announcement_pos, article_pos, announcement_last_data, article_last_data)
-            self.msg_send_process(text_announcement, text_article)
+        announcement_data, article_data, anno_today_sheet_data, type_trans_sheet_data = self.sheet_data_parsing()
+        type_trans_list = self.data_type_trans_check(anno_today_sheet_data)
+        anno_pos, article_pos = self.data_pos_check(announcement_data, article_data)
+        filtering_anno_data, filtering_article_data = self.data_filtering(anno_pos, article_pos, announcement_data[0][0])
+        text_data=self.set_text_process(type_trans_list, filtering_anno_data, filtering_article_data)
+        print(len(text_data))
+        if len(text_data) != 0:
+            self.msg_send_process(text_data)
             self.close()
-        else:
-            print('New data is not founded')
+            self.data_saving(filtering_anno_data, filtering_article_data, type_trans_list)
 
-def main(event, context):
+def main():
     dp=dataProcess()
     dp.routine()
+
+if __name__ == "__main__":
+    main()
